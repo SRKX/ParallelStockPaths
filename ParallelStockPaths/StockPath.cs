2a49d56e484e8fb56c1b626506444ea08ce5d4e2
Jeremie Smaga <jeremie@smaga.ch>
2013-01-12 01:07:18 +0000

    Initial Commit

---
+++ ParallelStockPaths/StockPath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+
+namespace ParallelStockPaths
+{
+    class StockPath
+    {
+
+        private double _lastPrice;
+        public double LastPrice
+        {
+            get
+            {
+                return _lastPrice;
+            }
+
+        }
+
+        public StockPath(int nbrSteps, double mean, double std)
+        {
+            double dt = 1;
+            Normal normal = Normal.WithMeanStdDev(0,1);
+            IEnumerable<double> returns = normal.Samples().Take(nbrSteps);
+            _lastPrice = returns.Aggregate(100.0, (st, wt) => (Math.Max(st + st * mean * dt + std * st * wt,0.0)));
+        }
+
+    }
+}
